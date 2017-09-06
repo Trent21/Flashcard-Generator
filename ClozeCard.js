4318de9c7c11863d35e9ab73492df9842fc725ba
Trent21 <tmitch.wall@gmail.com>
2017-09-06 15:56:33 -0400

    Update ClozeCard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -10,9 +10,9 @@ function ClozeCard (text, cloze, fulltext)
 var colorGrass = new ClozeCard("the color of grass is ", "green");
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
 
-console.log(grass.text); 
-console.log(grass.cloze); 
-console.log(grass.fulltext); 
+console.log(colorGrass.text); 
+console.log(colorGrass.cloze); 
+console.log(colorGrass.fulltext); 
 
 
 
